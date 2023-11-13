41e7b5e33f057e40e993bc4cec3187afc6c137fb
ismailouzy <ismailouzal@gmail.com>
2023-11-13 05:01:53 -0800

    added string fuction

---
+++ print_s.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_s - Prints a string
+ *
+ * @s: string parametre
+ *
+ * Return: Success
+ */ 
+
+	int
+print_s (char *s) 
+{
+	if (s == 0)
+	{
+		write (1, "(nil)", 5);
+	}
+	else
+	{
+		while (*s != '\0')
+		{
+			write(1, s, 1);
+			s++;
+		}
+
+	}
+	return (1);
+}
