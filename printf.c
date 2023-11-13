eae67c1c48bb5fe8b9538a17b0659be4ffa298be
ismailouzy <ismailouzal@gmail.com>
2023-11-13 06:19:54 -0800

    fixed printf 4

--- printf.c
+++ printf.c
@@ -1,59 +1,56 @@
 #include "main.h"
+
 /**
  * _printf - function that produces output according to a format
  *
  * @format: is a character string
  *
- * Return: Always 0
+ * Return: Success
  */
 int _printf(const char *format, ...)
 {
 	int i = 0;
 	va_list arg;
+	int count = 0;
 	char ch;
 	char *z;
 
 	va_start(arg, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
-	while (*format)
+	while (format && format[i])
 	{
-		if (*format != '%')
+		if (format[i] != '%')
 		{
-			write(1, format, 1);
-			i++;
+			write(1, &format[i], 1);
+			count++;
 		}
-		else if (*(format + 1) != '\0')
+		else if (format[i + 1] != '\0')
 		{
-			switch (*(format + 1))
+			switch (format[i + 1])
 			{
 				case 'c':
 					ch = va_arg(arg, int);
 					print_c(ch);
-					i += 2;
+					count++;
+					i++;
 					break;
 				case 's':
 					z = va_arg(arg, char *);
 					print_s(z);
-					i += 2;
+					i++;
 					break;
 				case '%':
 					write(1, "%", 1);
-					i += 2;
+					count++;
+					i++;
 					break;
 				default:
-					write(1, format, 1);
-					i++;
+					write(1, &format[i], 1);
+					count++;
 					break;
 			}
 		}
-		else
-		{
-			write(1, format, 1);
-			i++;
-		}
-		format++;
+		i++;
 	}
 	va_end(arg);
-	return (i);
+	return (count);
 }
