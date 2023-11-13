95e1843d4112e024645628f8d5e170fa66036d11
ismailouzy <ismailouzal@gmail.com>
2023-11-13 04:52:12 -0800

    prototypes main

--- main.h
+++ main.h
@@ -4,6 +4,9 @@
 #include <string.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdlib.h>
 int _printf(const char *format, ...);
-
+int print_c(char c);
+int print_s(char *s);
+int print_n(int n);
 #endif
